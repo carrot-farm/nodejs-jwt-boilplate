8ff23da3389807ea50c57af8c21fc617b4d5939f
carrot-farm <chohoki@gmail.com>
2020-03-07 05:43:03 +0900

    클러스트 모드 오터 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'api-server', // app 이름.
       script: './build/index.js', // 실행 스크립트
-      instance: 0, // cpu 갯수 만큼 인스턴스 실행
+      instances: 0, // cpu 갯수 만큼 인스턴스 실행
       exec_mode: 'cluster', // 클러스터 모드
       wait_ready: true, // 프로세스 종료 후 wait 이벤트가 발생하기 까지 기다린다.
       // max_memory_restart : "2G", // 최대 메모리 설정.
